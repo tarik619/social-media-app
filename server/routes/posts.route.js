85dc5e7e32f286eb805bc342228e6043f2c63b2f
tarik619 <itarik707@gmail.com>
2024-03-11 20:34:20 +0600

    created create a post, update post successfully

--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -1,5 +1,37 @@
 import express from "express";
+import { Post } from "../models/post.model.js";
 
 const router = express.Router();
+
+// create post
+
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// update post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (error) {
+    res.status(403).json(error);
+  }
+});
+// delete post
+// like post
+// get post
+// get timeline posts
 
 export default router;
