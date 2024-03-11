41436f6eedbd54ddfa1be72fe19dcfa6a90d7b85
tarik619 <itarik707@gmail.com>
2024-03-11 11:36:34 +0600

    making somechanges in user model like added desc,city,from,relationship, and created routes update user delete,get users

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,7 +1,57 @@
 import express from "express";
+import { User } from "../models/user.model.js";
 
 const router = express.Router();
 
-router.get("/", () => {});
+// update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      return res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can't update password");
+  }
+});
+// delete user
+
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      return res.status(200).json("Account has been deleted successfully");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update your own account");
+  }
+});
+
+// get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// follow a user
+// unfollow a user
 
 export default router;
