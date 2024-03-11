242ba8bddeaa408e31287601f4473f02990c6915
tarik619 <itarik707@gmail.com>
2024-03-11 20:08:28 +0600

    creating post model and add it to index

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import userRoute from "./routes/users.route.js";
 import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/posts.route.js";
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(3000, () => {
   console.log("backend server is running");
